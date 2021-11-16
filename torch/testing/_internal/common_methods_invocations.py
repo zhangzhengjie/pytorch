f625b96c800a09e605ea53f29ed9b0cd0875879f
Philip Meier <github.pmeier@posteo.de>
2021-11-16 15:05:26 +0100

    add `OpInfo`s for `torch.nn.functional.triplet_margin(_with_distance)?_loss`
    ghstack-source-id: ed1e97079b8f63189c91aa7b01a15ad46a2b0220
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/67079

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -9,6 +9,7 @@ import random
 import unittest
 
 import torch
+from torch import nn
 import numpy as np
 from torch._six import inf
 import collections.abc
@@ -7066,6 +7067,26 @@ def sample_inputs_binary_cross_entropy(op_info, device, dtype, requires_grad, lo
         for shape, kwargs in shapes_and_kwargs
     ]
 
+def sample_inputs_triplet_margin_loss(op_info, device, dtype, requires_grad, with_distance=False, **kwargs):
+    make = partial(make_tensor, (S, M), device=device, dtype=dtype, requires_grad=requires_grad)
+
+    kwargss = (
+        *[dict(margin=margin) for margin in (1e-6, 1.0, 10.0)],
+        # *[dict(p=p) for p in (0, 1, 2, 10, float("inf"))],
+        dict(swap=True),
+        *[dict(reduction=reduction) for reduction in ("mean", "sum", "none")],
+    )
+
+    sample_inputs = []
+    for kwargs in kwargss:
+        input = make()
+        args = (make(), make())
+        if with_distance:
+            kwargs["distance_function"] = nn.PairwiseDistance(p=kwargs.pop("p", 2.0), eps=kwargs.pop("eps", 1e-6))
+        sample_inputs.append(SampleInput(input, args=args, kwargs=kwargs))
+
+    return sample_inputs
+
 
 foreach_unary_op_db: List[OpInfo] = [
     ForeachFuncInfo('exp'),
@@ -13075,6 +13096,34 @@ op_db: List[OpInfo] = [
                 unittest.expectedFailure,
                 "TestJit",
                 "test_variant_consistency_jit",
+            ),
+        ),
+    ),
+    OpInfo(
+        "nn.functional.triplet_margin_loss",
+        sample_inputs_func=sample_inputs_triplet_margin_loss,
+        dtypesIfCPU=all_types_and_complex_and(torch.bfloat16),
+        dtypesIfCUDA=all_types_and_complex_and(torch.float16, torch.bfloat16),
+        supports_out=False,
+    ),
+    OpInfo(
+        "nn.functional.triplet_margin_with_distance_loss",
+        sample_inputs_func=partial(sample_inputs_triplet_margin_loss, with_distance=True),
+        dtypesIfCPU=all_types_and_complex_and(torch.bfloat16),
+        dtypesIfCUDA=all_types_and_complex_and(torch.float16, torch.bfloat16),
+        supports_out=False,
+        skips=(
+            DecorateInfo(
+                unittest.expectedFailure,
+                "TestJit",
+                "test_variant_consistency_jit",
+            ),
+            # This test cannot handle a callable passed to `distance_function`. If we would use
+            # `distance_function=None`, the test would pass fine.
+            DecorateInfo(
+                unittest.expectedFailure,
+                "TestNormalizeOperators",
+                "test_normalize_operator_exhaustive",
             ),
         ),
     )
